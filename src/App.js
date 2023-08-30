2d63688ee9ec0079604bfbcd1f2789fdc9022e13
JeremyMallet <jeremy.mallet@oclock.school>
2023-08-30 19:05:37 +0200

    Ajout et suppression de taches

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
 import './index.css';
 import Header from './components/Header/Header';
 import TaskContainer from './components/TaskContainer/TaskContainer';
+import React, { useState } from 'react';
 
 function App() {
+    const [tasks, setTasks] = useState([
+        { id: 1, title: 'Apprendre React' },
+        { id: 2, title: 'Faire les courses' },
+    ]);
+
     return (
         <div className="App">
             <Header />
-            <TaskContainer />
+            <TaskContainer tasks={tasks} setTasks={setTasks} />
         </div>
     );
 }
--- src/components/TaskContainer/TaskContainer.js
+++ src/components/TaskContainer/TaskContainer.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TaskContainer.css';
 
-function TaskContainer() {
+function TaskContainer({ tasks, setTasks }) {
+    const [newTaskTitle, setNewTaskTitle] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (newTaskTitle.trim() === '') return; // Evite l'ajout de tâches vides
+        
+        const newTask = {
+          id: Date.now(), // Utilise le timestamp comme ID unique
+          title: newTaskTitle
+        };
+      
+        setTasks([...tasks, newTask]); // Ajoute la nouvelle tâche à la liste
+        setNewTaskTitle(''); // Réinitialise l'input
+      }
+
+    const handleDelete = (id) => {
+        const updatedTasks = tasks.filter((task) => task.id !== id);
+        setTasks(updatedTasks);
+    };
+
     return (
-        <div className="task-container">
-            <div className="box task">
-                <p className="task-title">Apprendre React</p>
+        <> 
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Nouvelle tâche"
+                    value={newTaskTitle}
+                    onChange={(e) => setNewTaskTitle(e.target.value)}
+                />
+                <button type="submit">Ajouter</button>
+            </form>
+            <div className="task-container">
+                {tasks.map((task) => (
+                    <div key={task.id} className="box task">
+                        <p className="task-title">{task.title}</p>
+                        <button onClick={() => handleDelete(task.id)}>Supprimer</button>
+                    </div>
+                ))}
             </div>
-            <div className="box task">
-                <p className="task-title">Faire les courses</p>
-            </div>
-        </div>
+        </> 
     );
 }
 
